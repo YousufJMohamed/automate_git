f93c8676a4d41c40aa23784a734e6a9575531776
YousufJMohamed <mohamedyousuf1136@gmail>
2021-09-08 00:23:18 +0530

    Updates for 2021-09-08

--- git.sh
+++ git.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd tact/Dailylogs
+cd C:\Users\mohamed yousuf\Desktop\placment preperation\project\Automate git push\automate_git
 git add .
 echo "added"
 git commit -m "Updates for $(date +%F)"
